3ba1b4e880e8548b3d3067b1793d783d2ea8471c
David Toro Alzate <dtoro@cognox.com>
2016-10-13 11:49:25 -0500

    punteros añadidos automaticamente

--- reloj.js
+++ reloj.js
@@ -1,13 +1,57 @@
 var $all = $('body');
 
 var $circle = $('<div>');
-$circle.css({"background-color":"green",
+$circle.css({"background-color":"#ffa726",
             "border-radius" : "100%",
             "height" : "300px",
             "width": "300px"
 });
 
+var $table = $('<table>').css("borderCollapse", "collapse");
 
+
+for (i = 0; i < 15; i++) {
+    $table.append($('<tr>'));
+
+}
+
+for(i = 0; i < 15; i++){
+    $table.find('tr').append($('<td>').css({
+        "height" : "20px",
+        "width": "20px",
+        "padding" : "0",
+        "font-weight" : "bold",
+        "text-align" : "center"
+        }));
+
+}
+
+var marks = [7, 105, 119, 217];
+
+for(i = 0; i <= marks.length; i++){
+    $table.find('tr td').eq(marks[i]).append($('<div>')
+        .css({"background" : "#00c853",
+            "border-radius" : "100%",
+            "width" : "20px",
+            "height" : "20px"}));
+}
+
+var marks2 = [ 19, 25, 61, 73, 112, 151, 163, 199, 205];
+
+for(i = 0; i <= marks2.length; i++){
+    $table.find('tr td').eq(marks2[i]).append($('<div>')
+        .css({"background" : "#00c853",
+            "border-radius" : "100%",
+
+            "width" : "16px",
+            "height" : "16px"}));
+}
+
+// for(i = 0; i )
+
+
+    // "transform" : "rotate(60deg)",
 
 $all.append($circle);
 
+$circle.append($table);
