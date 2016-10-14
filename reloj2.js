0cd4f6a880834a3fdd32deb5f7713e56c41d8f00
David Toro Alzate <David Toro Alzate>
2016-10-13 20:42:42 -0500

    trabajo en casa

---
+++ reloj2.js
@@ -0,0 +1,16 @@
+// var $all = $('body');
+
+// var $circle = $('<div>').css({"background-color": "purple",
+// 								"width" : "400px",
+// 								"height" : "400px",
+// 								"border-radius" : "100%"});
+
+
+// var $lines = $('<div>').css({"background-color" : "black",
+// 							"height" : "390px",
+// 							"width" : "20px"})
+
+
+// $circle.append($lines).css({"text-align" : "center"});
+
+// $all.append($circle);
