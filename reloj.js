a25af371de0f0a8a77a64ac1ec283b539b78eb83
David Toro Alzate <dtoro@cognox.com>
2016-10-14 10:57:20 -0500

    Primera finalización Reloj

--- reloj.js
+++ reloj.js
@@ -1,20 +1,20 @@
 // var $all = $('body');
-
+//
 // var $circle = $('<div>');
 // $circle.css({"background-color":"#ffa726",
 //             "border-radius" : "100%",
 //             "height" : "300px",
 //             "width": "300px"
 // });
-
+//
 // var $table = $('<table>').css("borderCollapse", "collapse");
-
-
+//
+//
 // for (i = 0; i < 15; i++) {
 //     $table.append($('<tr>'));
-
+//
 // }
-
+//
 // for(i = 0; i < 15; i++){
 //     $table.find('tr').append($('<td>').css({
 //         "height" : "20px",
@@ -23,11 +23,11 @@
 //         "font-weight" : "bold",
 //         "text-align" : "center"
 //         }));
-
+//
 // }
-
+//
 // var marks = [7, 105, 119, 217];
-
+//
 // for(i = 0; i <= marks.length; i++){
 //     $table.find('tr td').eq(marks[i]).append($('<div>')
 //         .css({"background" : "#00c853",
@@ -35,23 +35,19 @@
 //             "width" : "20px",
 //             "height" : "20px"}));
 // }
-
+//
 // var marks2 = [ 19, 25, 61, 73, 112, 151, 163, 199, 205];
-
+//
 // for(i = 0; i <= marks2.length; i++){
 //     $table.find('tr td').eq(marks2[i]).append($('<div>')
 //         .css({"background" : "#00c853",
 //             "border-radius" : "100%",
-
+//
 //             "width" : "16px",
 //             "height" : "16px"}));
 // }
-
-// // for(i = 0; i )
-
-
-//     // "transform" : "rotate(60deg)",
-
+//
+//
 // $all.append($circle);
-
+//
 // $circle.append($table);
--- reloj2.js
+++ reloj2.js
@@ -1,16 +1,113 @@
-// var $all = $('body');
+var $all = $('body').css("background","grey");
 
-// var $circle = $('<div>').css({"background-color": "purple",
-// 								"width" : "400px",
-// 								"height" : "400px",
-// 								"border-radius" : "100%"});
-
-
-// var $lines = $('<div>').css({"background-color" : "black",
-// 							"height" : "390px",
-// 							"width" : "20px"})
+var $circle1 = $('<div>').css({
+                            "background-color": "purple",
+                            "height": "400px",
+                            "width": "400px",
+                            "border-radius": "100%",
+                            "position": "relative"
+                            });
 
 
-// $circle.append($lines).css({"text-align" : "center"});
+var $circle2 = $circle1.clone();
+$circle2.css({
+    "height": "340px",
+    "width": "340px",
+    "left": "7%",
+    "top": "7%"
+});
 
-// $all.append($circle);
+var $circle3 =  $('<div>').css({
+    "background-color": "black",
+    "height": "35px",
+    "width": "35px",
+    "border-radius": "100%",
+    "position": "absolute",
+    "top" : "47%",
+    "left" : "46%",
+    'transform' : 'rotate(180deg)'
+});
+
+var $circle4 = $circle3.clone();
+$circle4.css("background" , "transparent");
+
+var $circle5 = $circle4.clone();
+
+
+var width = ["20px", "10px", "5px"];
+var left = ["190px", "195px"];
+var transform = 0;
+
+var X = 0;
+
+
+for(i=0; i<=5; i++){
+    if(i==0 || i==3){
+        X = 0;
+    }else{
+        X = 1;
+    }
+    var $temp = $('<div>').css({
+        "background-color": "black",
+        "height": "400px",
+        "width": width[X],
+        "position": "absolute",
+        "left": left[X]
+    });
+    $temp.css("transform", 'rotate( '+ (transform) +'deg)');
+    transform+=30;
+    $circle1.append($temp);
+}
+
+
+var $hand1 = $('<div>').css({
+        'background-color': 'black',
+        'height': '160px' ,
+        'width': '20px' ,
+        'position': 'absolute',
+        'left': '25%',
+        'top' : '15%'
+});
+
+var $hand2 = $hand1.clone()
+$hand2.css({
+    "height" : "120px",
+    "width" : "16px"
+});
+
+var $hand3 = $hand1.clone()
+$hand3.css({
+    "height" : "160px",
+    "width" : "12px"
+});
+
+$circle5.append($hand3);
+$circle4.append($hand2);
+$circle3.append($hand1);
+$circle1.append($circle2);
+$circle1.append($circle3);
+$circle1.append($circle4);
+$circle1.append($circle5);
+$all.append($circle1);
+
+var S = 186;
+var sec = 0;
+var M = 186;
+var T  = 180;
+
+
+
+var timeH = setInterval(function(){
+
+    $circle5.css("transform" , 'rotate('+ S +'deg)');
+    S+=6;
+    sec++;
+    if(sec==60){
+        $circle3.css("transform" , 'rotate('+ M +'deg)');
+        M+=6;
+        sec=0;
+        $circle4.css("transform" , 'rotate('+ T +'deg)');
+        T+=0.5;
+    }
+
+}, 1000);
